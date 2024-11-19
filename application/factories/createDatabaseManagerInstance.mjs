bf00b071a17dd974dd927d5ad7bfc4a5fc1ef27e
Glenn Hall <glennerichall@hotmail.com>
2024-11-18 23:33:07 -0500

    removed ssl disabling on test in connection string

--- application/factories/createDatabaseManagerInstance.mjs
+++ application/factories/createDatabaseManagerInstance.mjs
@@ -1,7 +1,6 @@
 import {
     getEnvValue,
     getEnvValueIndirect,
-    getNodeEnv,
     getProvider
 } from "velor-services/injection/baseServices.mjs";
 import {
@@ -9,7 +8,6 @@ import {
     DATABASE_SCHEMA,
     DATABASE_URL_VAR
 } from "../services/databaseEnvKeys.mjs";
-import {ENV_TEST} from "velor-utils/env.mjs";
 import {DatabaseManager} from "../../database/DatabaseManager.mjs";
 import {s_databaseStatements} from "../services/databaseServiceKeys.mjs";
 import {PoolManager} from "../../database/PoolManager.mjs";
@@ -18,10 +16,6 @@ export function createDatabaseManagerInstance(services) {
     let schema = getEnvValue(services, DATABASE_SCHEMA);
     let connectionString = getEnvValue(services, DATABASE_CONNECTION_STRING) ??
         getEnvValueIndirect(services, DATABASE_URL_VAR);
-
-    if (getNodeEnv(services) === ENV_TEST) {
-        connectionString += "?sslmode=disable";
-    }
 
     const pool = new PoolManager(connectionString);
 
