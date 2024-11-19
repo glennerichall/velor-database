06670da9506acc5f9efec9d8d5de7045e8e1d7c8
Glenn Hall <glennerichall@hotmail.com>
2024-11-18 20:20:34 -0500

    added createDatabase for postgre

--- database/queryRaw.mjs
+++ database/queryRaw.mjs
@@ -1,6 +1,7 @@
 import {logQuery} from "./logQuery.mjs";
+import {noOpLogger} from "velor-utils/utils/noOpLogger.mjs";
 
-export async function queryRaw(client, query, args, logger) {
+export async function queryRaw(client, query, args, logger = noOpLogger) {
     try {
         const res = await client.query(query, args);
         return res.rows;
